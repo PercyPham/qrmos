71f954bb23d0ae0a3ae274d872811ad93f057898
Percy Pham <hungpm.percy@gmail.com>
2022-03-01 14:50:19 +0700

    chore(be): mock code for momo prod test

--- backend/internal/adapter/controller/api_order_payment.go
+++ backend/internal/adapter/controller/api_order_payment.go
@@ -1,6 +1,10 @@
 package controller
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"qrmos/internal/adapter/controller/internal/response"
 	"qrmos/internal/common/apperror"
 	"qrmos/internal/entity"
@@ -71,4 +75,37 @@ func (s *server) createMoMoPaymentLinkAsStaff(t time.Time, c *gin.Context, order
 		return
 	}
 	response.Success(c, paymentLink)
+}
+
+func (s *server) handleMoMoIpnCallback(c *gin.Context) {
+	printReqInfo(c, "handleMoMoIpnCallback")
+	response.Success(c, true)
+}
+
+func (s *server) handleMoMoPaymentCallback(c *gin.Context) {
+	printReqInfo(c, "handleMoMoPaymentCallback")
+	response.Success(c, true)
+}
+
+// TODO: this one is for testing MoMo callback. Should be deleted later.
+func printReqInfo(c *gin.Context, handlerName string) {
+	m := map[string]interface{}{}
+
+	m["requestor"] = c.Request.RemoteAddr
+	m["method"] = c.Request.Method
+	m["host"] = c.Request.URL.Host
+	m["rawPath"] = c.Request.URL.RawPath
+	m["path"] = c.Request.URL.Path
+	m["queries"] = c.Request.URL.Query()
+	if c.Request.Method == http.MethodPost {
+		bodyAsByteArray, _ := ioutil.ReadAll(c.Request.Body)
+		jsonMapBody := map[string]interface{}{}
+		_ = json.Unmarshal(bodyAsByteArray, &jsonMapBody)
+		m["body"] = jsonMapBody
+	}
+
+	jsonResult, _ := json.Marshal(m)
+	fmt.Println(">>>>>>>")
+	fmt.Println(string(jsonResult))
+	fmt.Println(">>>>>>>")
 }
