01c50b1f4820a61175e39bf4800e7210a701d57c
Percy Pham <hungpm.percy@gmail.com>
2022-03-10 12:51:44 +0700

    refactor(fe): utilize api bool response

--- frontend/lib/services/qrmos/delivery/delete_dest.dart
+++ frontend/lib/services/qrmos/delivery/delete_dest.dart
@@ -1,16 +1,7 @@
 import '../utils/utils.dart';
 
-Future<DeleteDestResponse> deleteDest(String destName) async {
+Future<ApiBoolResponse> deleteDest(String destName) async {
   var apiRawResp = await delete('/delivery-destinations/$destName');
-  var resp = DeleteDestResponse.fromJson(apiRawResp);
+  var resp = ApiBoolResponse.fromJson(apiRawResp);
   return resp;
-}
-
-class DeleteDestResponse {
-  bool? data;
-  ApiError? error;
-
-  DeleteDestResponse.fromJson(ApiResponse apiResp)
-      : error = apiResp.error,
-        data = apiResp.dataJson == true ? true : false;
 }
--- frontend/lib/services/qrmos/user/create_user.dart
+++ frontend/lib/services/qrmos/user/create_user.dart
@@ -1,22 +1,13 @@
 import '../utils/utils.dart';
 import './models.dart';
 
-Future<CreateUserResponse> createUser(User user) async {
+Future<ApiBoolResponse> createUser(User user) async {
   var apiRawResp = await post("/users", body: {
     "username": user.username,
     "password": user.password,
     "fullName": user.fullName,
     "role": user.role,
   });
-  var resp = CreateUserResponse.fromJson(apiRawResp);
+  var resp = ApiBoolResponse.fromJson(apiRawResp);
   return resp;
-}
-
-class CreateUserResponse {
-  bool? data;
-  ApiError? error;
-
-  CreateUserResponse.fromJson(ApiResponse apiResp)
-      : data = apiResp.dataJson == true ? true : false,
-        error = apiResp.error;
 }
